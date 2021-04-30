e87f8ad3f08a2ef434f4b64e332306718b52fb9b
Agie <boromeus.agie@gmail.com>
2021-04-30 16:00:13 +0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <link rel="stylesheet" href="css/app.css">
+
         <!-- Styles -->
         <style>
             html, body {
@@ -65,7 +67,7 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+            {{-- @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
@@ -77,22 +79,19 @@
                         @endif
                     @endauth
                 </div>
-            @endif
+            @endif --}}
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    TOKO ANDATU 5758
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="link">
+                    @auth
+                        <a class="btn btn-success btn-lg text-white" href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a class="btn btn-primary btn-lg text-white" href="{{ route('login') }}">Login</a>
+                    @endauth
                 </div>
             </div>
         </div>
